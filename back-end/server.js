25edb89148bd3ad852905eff82baa8edd2752463
LauraHTML <laura.senai03@gmail.com>
2026-02-25 18:15:51 -0300

    Adição de adição de um novo snipped, busca por id dentro da url

--- back-end/server.js
+++ back-end/server.js
@@ -2,16 +2,6 @@ import app from "./src/app.js";
 
 const porta = 3000;
 
-const codigos = {
-    id: 1, 
-    codigo: 'console.log("hello world")',
-    linguagem: 'javaScript'
-}
-
-app.get("/codigos", (req,res) => {
-    res.status(200).json(codigos)
-})
-
 app.listen(porta, () => {
   console.log("servidor rodando!");
 });
--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,6 +3,31 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+const codigos = {
+    id: 1, 
+    codigo: 'console.log("hello world")',
+    linguagem: 'javaScript',
+    id: 2, 
+    codigo: '$dia = segunda',
+    linguagem: 'php',
+}
+
+app.use(express.json());
+
+app.get("/codigos", (req,res) => {
+    res.status(200).json(codigos)
+})
+
+app.post("/codigos", (req, res) => {
+  codigos.push(req.body);
+  res.status(201).send("codigo adicionado com sucesso");
+});
+
+app.get("/codigos/:id", (req, res) => {
+  const index = buscaCodigo(req.params.id);
+  res.status(200).json(livros[index]);
+})
+
 app.get("/", (req, res) => {
   res.status(200).send("Servidor inicial");
 });
