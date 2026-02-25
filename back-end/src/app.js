68784d5c043e7680f5601b15789e7fc40202ed36
LauraHTML <laura.senai03@gmail.com>
2026-02-25 18:35:13 -0300

    refazendo função de buscar codigo por id

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,33 +3,45 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const codigos = {
-    id: 1, 
-    codigo: 'console.log("hello world")',
-    linguagem: 'javaScript',
-    id: 2, 
-    codigo: '$dia = segunda',
-    linguagem: 'php',
-}
+const codigos = [
+    {
+        id: 1,
+        codigo: 'console.log("hello world")',
+        linguagem: 'javaScript',
+    },
+    {
+        id: 2,
+        codigo: '$dia = segunda',
+        linguagem: 'php',
+    }
+]
+
+
 
 app.use(express.json());
 
-app.get("/codigos", (req,res) => {
+app.get("/codigos", (req, res) => {
     res.status(200).json(codigos)
 })
 
 app.post("/codigos", (req, res) => {
-  codigos.push(req.body);
-  res.status(201).send("codigo adicionado com sucesso");
+    codigos.push(req.body);
+    res.status(201).send("codigo adicionado com sucesso");
 });
 
+function buscarCodigo(id) {
+    return codigos.findIndex(codigo => {
+        return codigo.id === Number(id);
+    })
+}
+
 app.get("/codigos/:id", (req, res) => {
-  const index = buscaCodigo(req.params.id);
-  res.status(200).json(livros[index]);
+    const index = buscarCodigo(req.params.id);
+    res.status(200).json(codigos[index]);
 })
 
 app.get("/", (req, res) => {
-  res.status(200).send("Servidor inicial");
+    res.status(200).send("Servidor inicial");
 });
 
 export default app;
