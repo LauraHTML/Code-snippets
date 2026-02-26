6c8b53c91cb9f8ecc5ba3d6cd28a8c7b6f04cecc
LauraHTML <laura.senai03@gmail.com>
2026-02-26 16:31:46 -0300

    Criação do schema do código

---
+++ back-end/src/models/Codigo.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const codigosSchema = new mongoose.Schema({
+  id: { type: mongoose.Schema.Types.ObjectId },
+  titulo: { type: String, required: true },
+  codigo: { type: String },
+  tags: { type: [String] },
+  favoritado: { type: Boolean },
+  dataCriacao: { type: Date }
+}, { versionKey: false });
+
+const codigo = mongoose.model("codigos", codigosSchema);
+
+export default codigo;
